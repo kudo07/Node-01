f37e365824413b8a6047db18b9798f6f15ab0b8f
kudo07 <134780035+kudo07@users.noreply.github.com>
2023-12-05 17:18:30 +0530

    first commit

---
+++ feature.js
@@ -0,0 +1,10 @@
+// const name = "hello";
+// const name1 = "next hello";
+// const name2 = "next next hello";
+// export default name;
+// // module.exports = name;
+// export { name, name1, name2 };
+
+// export const generateLovePercent = () => {
+//   return `${Math.random * 100}%`;
+// };
